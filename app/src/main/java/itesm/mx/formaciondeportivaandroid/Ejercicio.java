a3ce4888a1bd1c49526a5c7036c6083e9ede1b04
Rolando Ruiz <Rolando Ruiz>
2016-10-24 11:01:12 -0500

    Se añadio la licencia de software libre

--- app/src/main/java/itesm/mx/formaciondeportivaandroid/Ejercicio.java
+++ app/src/main/java/itesm/mx/formaciondeportivaandroid/Ejercicio.java
@@ -1,10 +1,23 @@
 package itesm.mx.formaciondeportivaandroid;
 
-import java.util.ArrayList;
+/*
+* Copyright (c) 2016, Instituto Tecnológico y de Estudios Superiores de Monterrey, México. Derechos reservados.
+* DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses.
+*/
 
-/**
- * Created by armando on 23/10/2016.
- */
 public class Ejercicio {
     private String sNombreEjer;
     private int iSeries;
